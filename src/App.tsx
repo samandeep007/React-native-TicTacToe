2c2e3b3db0fbce2f8c81063baff61083f292d67f
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-04 22:10:18 -0600

    chore: Update react-native-snackbar dependency to version 2.6.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
-import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native'
-import Snackbar from 'react-native-snackbar'
-import Icons from './components/Icons'
-import React, {useState} from 'react'
 
-export default function App() {
-  const[isCross, setIsCross] = useState<boolean>(false);
-  const[gameWinner, setGameWinner] = useState<string>('');
-  const[gameState, setGameState] = useState(new Array(9).fill('empty', 0, 9))
+import React, { useState } from 'react';
+
+import {
+  FlatList,
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import Snackbar from 'react-native-snackbar';
+import Icons from './components/Icons';
+
+function App(): JSX.Element {
+  const [isCross, setIsCross] = useState<boolean>(false)
+  const [gameWinner, setGameWinner] = useState<string>('')
+  const [gameState, setGameState] = useState(new Array(9).fill('empty', 0, 9))
 
   const reloadGame = () => {
-    setIsCross(false);
-    setGameWinner('');
-    setGameState(new Array(9).fill('empty',0,9));
+    setIsCross(false)
+    setGameWinner('')
+    setGameState(new Array(9).fill('empty', 0, 9))
   }
 
   const checkIsWinner = () => {
@@ -68,43 +81,146 @@ export default function App() {
       setGameWinner('Draw game... ⌛️');
     }
   }
-
   const onChangeItem = (itemNumber: number) => {
-    if(gameWinner){
+    if (gameWinner) {
       return Snackbar.show({
         text: gameWinner,
-        backgroundColor: "#000000",
+        backgroundColor: '#000000',
         textColor: "#FFFFFF"
       })
     }
 
-    if(gameState[itemNumber] === "empty"){
-      gameState[itemNumber] = isCross ? 'cross': 'circle';
-      setIsCross(!isCross);
-    }
-    else {
+    if (gameState[itemNumber] === 'empty') {
+      gameState[itemNumber] = isCross ? 'cross': 'circle'
+      setIsCross(!isCross)
+    } else {
       return Snackbar.show({
         text: "Position is already filled",
         backgroundColor: "red",
-        textColor: "white"
+        textColor: "#FFF"
       })
     }
 
-    checkIsWinner();
+    checkIsWinner()
   }
 
-  console.log(gameState)
- 
   return (
-   <SafeAreaView>
-    <StatusBar/>
-    <View>
-      <Text>
-        TicTacToe with Saman Sandhu
-      </Text>
-    </View>
-   </SafeAreaView>
-  )
+    <SafeAreaView >
+      <StatusBar />
+      {gameWinner ? (
+        <View style={[styles.playerInfo, styles.winnerInfo]}>
+          <Text style={styles.winnerTxt}>{gameWinner}</Text>
+        </View>
+      ) : (
+        <View
+        style={[
+          styles.playerInfo,
+          isCross ? styles.playerX : styles.playerO
+        ]}
+        >
+          <Text style={styles.gameTurnTxt}>
+            Player {isCross? 'X' : 'O'}'s Turn
+          </Text>
+        </View>
+      )}
+      {/* Game Grid */}
+      <FlatList
+      numColumns={3}
+      data={gameState}
+      style={styles.grid}
+      renderItem={({item, index}) => (
+        <Pressable
+        key={index}
+        style={styles.card}
+        onPress={() => onChangeItem(index)}
+        >
+          <Icons name={item} />
+        </Pressable>
+      )}
+      />
+      {/* game action */}
+      <Pressable
+      style={styles.gameBtn}
+      onPress={reloadGame}
+      >
+        <Text style={styles.gameBtnText}>
+          {gameWinner ? 'Start new game' : 'reLoad the game'}
+        </Text>
+      </Pressable>
+    </SafeAreaView>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  playerInfo: {
+    height: 56,
+
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    borderRadius: 4,
+    paddingVertical: 8,
+    marginVertical: 12,
+    marginHorizontal: 14,
+
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: '#333',
+    shadowOpacity: 0.2,
+    shadowRadius: 1.5,
+  },
+  gameTurnTxt: {
+    fontSize: 20,
+    color: '#FFFFFF',
+    fontWeight: '600',
+  },
+  playerX: {
+    backgroundColor: '#38CC77',
+  },
+  playerO: {
+    backgroundColor: '#F7CD2E',
+  },
+  grid: {
+    margin: 12,
+  },
+  card: {
+    height: 100,
+    width: '33.33%',
+
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    borderWidth: 1,
+    borderColor: '#333',
+  },
+  winnerInfo: {
+    borderRadius: 8,
+    backgroundColor: '#38CC77',
+
+    shadowOpacity: 0.1,
+  },
+  winnerTxt: {
+    fontSize: 20,
+    color: '#FFFFFF',
+    fontWeight: '600',
+    textTransform: 'capitalize',
+  },
+  gameBtn: {
+    alignItems: 'center',
+
+    padding: 10,
+    borderRadius: 8,
+    marginHorizontal: 36,
+    backgroundColor: '#8D3DAF',
+  },
+  gameBtnText: {
+    fontSize: 18,
+    color: '#FFFFFF',
+    fontWeight: '500',
+  },
+});
+
+export default App;
