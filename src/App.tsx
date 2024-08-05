fea10d8d01c90de6f6da26780097b44480300f30
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-04 22:05:58 -0600

    chore: Update react-native-snackbar dependency to version 2.6.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,109 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native'
+import Snackbar from 'react-native-snackbar'
+import Icons from './components/Icons'
+import React, {useState} from 'react'
 
 export default function App() {
+  const[isCross, setIsCross] = useState<boolean>(false);
+  const[gameWinner, setGameWinner] = useState<string>('');
+  const[gameState, setGameState] = useState(new Array(9).fill('empty', 0, 9))
+
+  const reloadGame = () => {
+    setIsCross(false);
+    setGameWinner('');
+    setGameState(new Array(9).fill('empty',0,9));
+  }
+
+  const checkIsWinner = () => {
+    //  checking  winner of the game
+    if (
+      gameState[0] === gameState[1] &&
+      gameState[0] === gameState[2] &&
+      gameState[0] !== 'empty'
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🥳`);
+    } else if (
+      gameState[3] !== 'empty' &&
+      gameState[3] === gameState[4] &&
+      gameState[4] === gameState[5]
+    ) {
+      setGameWinner(`${gameState[3]} won the game! 🥳`);
+    } else if (
+      gameState[6] !== 'empty' &&
+      gameState[6] === gameState[7] &&
+      gameState[7] === gameState[8]
+    ) {
+      setGameWinner(`${gameState[6]} won the game! 🥳`);
+    } else if (
+      gameState[0] !== 'empty' &&
+      gameState[0] === gameState[3] &&
+      gameState[3] === gameState[6]
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🥳`);
+    } else if (
+      gameState[1] !== 'empty' &&
+      gameState[1] === gameState[4] &&
+      gameState[4] === gameState[7]
+    ) {
+      setGameWinner(`${gameState[1]} won the game! 🥳`);
+    } else if (
+      gameState[2] !== 'empty' &&
+      gameState[2] === gameState[5] &&
+      gameState[5] === gameState[8]
+    ) {
+      setGameWinner(`${gameState[2]} won the game! 🥳`);
+    } else if (
+      gameState[0] !== 'empty' &&
+      gameState[0] === gameState[4] &&
+      gameState[4] === gameState[8]
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🥳`);
+    } else if (
+      gameState[2] !== 'empty' &&
+      gameState[2] === gameState[4] &&
+      gameState[4] === gameState[6]
+    ) {
+      setGameWinner(`${gameState[2]} won the game! 🥳`);
+    } else if (!gameState.includes('empty', 0)) {
+      setGameWinner('Draw game... ⌛️');
+    }
+  }
+
+  const onChangeItem = (itemNumber: number) => {
+    if(gameWinner){
+      return Snackbar.show({
+        text: gameWinner,
+        backgroundColor: "#000000",
+        textColor: "#FFFFFF"
+      })
+    }
+
+    if(gameState[itemNumber] === "empty"){
+      gameState[itemNumber] = isCross ? 'cross': 'circle';
+      setIsCross(!isCross);
+    }
+    else {
+      return Snackbar.show({
+        text: "Position is already filled",
+        backgroundColor: "red",
+        textColor: "white"
+      })
+    }
+
+    checkIsWinner();
+  }
+
+  console.log(gameState)
+ 
   return (
+   <SafeAreaView>
+    <StatusBar/>
     <View>
-      <Text>Appxcx</Text>
+      <Text>
+        TicTacToe with Saman Sandhu
+      </Text>
     </View>
+   </SafeAreaView>
   )
 }
 
